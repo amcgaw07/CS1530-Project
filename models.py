7a1fc993ea11223274d59f4ff29fa36a047cb41f
bmj-34 <77993602+bmj-34@users.noreply.github.com>
2021-04-18 14:14:34 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -4,19 +4,6 @@ from sqlalchemy.sql.expression import func
 from datetime import date
  
 db = SQLAlchemy()
-
-class Admin(db.Model):
-	__tablename__ = 'admin'
-	admin_id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(30), unique=True)
-	password = db.Column(db.String(30), unique=True)
-	
-	def __init__(self, username, password):
-		self.username = username
-		self.password = password
-	
-	def __repr__(self):
-		return '<Admin {}>'.format(self.username)
 		
 class User(db.Model):
 	__tablename__ = 'user'
--- routes.py
+++ routes.py
@@ -8,7 +8,7 @@ import os
 from hashlib import md5
 from datetime import date
 from werkzeug.security import check_password_hash, generate_password_hash
-from models import db, Admin, User, Favorite, Subscription
+from models import db, User, Favorite, Subscription
 import tmdbsimple as tmbd
 import json
 tmbd.API_KEY='9d442b83bb8972605022892d3c12fb0e'
@@ -39,15 +39,7 @@ def initdb_command():
 	"""Creates the database tables."""
 	db.drop_all()
 	db.create_all()
-	admin_user = Admin(username="admin", password="pass")
-	db.session.add(admin_user)
-	db.session.commit()
 	print('Initialized the database.')
-
-def get_admin_id(username):
-	"""Convenience method to look up the id for a username."""
-	rv = Admin.query.filter_by(username=username).first()
-	return rv.admin_id if rv else None
 
 def get_user_id(username):
 	"""Convenience method to look up the id for a username."""
@@ -57,9 +49,6 @@ def get_user_id(username):
 @app.before_request
 def before_request():
 	g.user = None
-	g.admin = None
-	if 'admin_id' in session:
-		g.admin = Admin.query.filter_by(admin_id=session['admin_id']).first()
 	if 'user_id' in session:
 		g.user = User.query.filter_by(user_id=session['user_id']).first()
 	
@@ -77,8 +66,6 @@ def register():
 			error = 'You have to enter a password'
 		elif request.form['password'] != request.form['password2']:
 			error = 'The two passwords do not match'
-		elif get_admin_id(request.form['username']) is not None:
-			error = 'The username is already taken'
 		elif get_user_id(request.form['username']) is not None:
 			error = 'The username is already taken'
 		else:
@@ -94,11 +81,7 @@ def login():
 	"""Logs the user in."""
 	error = None
 	if request.method == 'POST':
-		if not Admin.query.filter_by(username=request.form['username'], password=request.form['password']).first() is None:
-			session['admin_id'] = Admin.query.filter_by(username=request.form['username']).first().admin_id
-			flash('You were logged in')
-			return redirect(url_for('admin'))
-		elif not User.query.filter_by(username=request.form['username']).first() is None:
+		if not User.query.filter_by(username=request.form['username']).first() is None:
 			user = User.query.filter_by(username=request.form['username']).first()
 			if check_password_hash(user.password, request.form['password']):
 				session['user_id'] = User.query.filter_by(username=request.form['username']).first().user_id
@@ -173,15 +156,6 @@ def logoutUser():
 	flash('You were logged out')
 	session.pop('user_id', None)
 	return redirect(url_for('login'))
-
-@app.route('/logoutAdmin')
-def logoutAdmin():
-	"""Logs the admin out."""
-	flash('You were logged out')
-	session.pop('admin_id', None)
-	return redirect(url_for('login'))
-
-
 
 @app.route('/')
 @app.route('/index.html',methods=['GET','POST'])
